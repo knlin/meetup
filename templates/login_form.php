415756188c8dd2e427bbe10e3232c952c1a19e7b
Andy Shi <andyshi@college.harvard.edu>
2013-02-03 12:51:32 -0500

    cosmetic changes to website

--- templates/login_form.php
+++ templates/login_form.php
@@ -21,7 +21,7 @@
 });
 
 </script>
-
+<div id="login" style="width:50%; float:right; padding-top:150px">
 <form action="login.php" method="post" id="login">
     <fieldset>
         <div class="control-group">
@@ -35,7 +35,17 @@
         </div>
     </fieldset>
 </form>
-<div>
+
     <p id="error"></p>
     or <a href="register.php">register</a> for an account
+
+</div>
+<div id="description" style="width:50%; float:right">
+    <p style="font-size:20px; line-height:1.5em">tickets for two is a service created for the movie lover in you. We'll match you with someone who has similar tastes in movies and leave you to enjoy each other's company over a good chop-dropping cop flick or your favorite rom com drom. Sound like a good time? We think so, too. Sign up for free to give us a spin!</p>
+</div>
+<div style="clear:both">
+<br />	
+<hr />
+<p style="font-size:30px">How it works:</p>
+    <img src="../img/illustration_expanded.jpg"></img>
 </div>
