0aaf30ed91fa7b8bc93f9a0f05f89fde538e9ff3
Andy Shi <andyshi@college.harvard.edu>
2013-02-03 11:50:15 -0500

    added styling and pictures

--- templates/date_list.php
+++ templates/date_list.php
@@ -2,21 +2,26 @@
     <thead>
         <tr>
             <th>Your date</th>
+            <th>Contact</th>
             <th>Movie</th>
+            <th>&nbsp;</th>
         </tr>
     </thead>
     <tbody>
     
     <?php
-    //dump($date_id);
+    //dump($email);
     if (empty($date_id))
     {
         echo '<p>Sorry, you do not have any matches at this time</p>';
     }
     else
     {
-        echo '<tr><td>' . $date_id["name"] . '</td>';
-        echo '<td><a href="http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dmovies-tv&field-keywords=' . htmlspecialchars($movie) . '">' . $movie . '</a></td></tr>';
+        echo '<tr><td>' . htmlspecialchars($date_id) . '</td>';
+        echo '<td>' . htmlspecialchars($email) . '</td>';
+        echo '<td><a href="http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dmovies-tv&field-keywords=' . htmlspecialchars($movie) . '">' . $movie . '</a></td>';
+        echo '<td>You have offered to pay</td>';
+        echo '</tr>';
     } 
     ?>
     </tbody>
